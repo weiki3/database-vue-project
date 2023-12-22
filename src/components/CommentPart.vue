2fda78328cc4cd01653dc587c8d5925f6b0006b1
weiki <wikii3@outlook.com>
2023-12-22 18:16:43 +0800

    v0.0.7 add comment part not ready use

--- src/components/CommentPart.vue
+++ src/components/CommentPart.vue
@@ -1,14 +1,83 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
+import useUserStore from "../stores/user"
 import axios from 'axios';
 
 const props = defineProps(['vid'])
+const comments = ref([]);
+const myComment = reactive({
+    content: '',
+    point: ''
+})
 
+const userStore = useUserStore()
+
+function getComments() {
+    axios.get(`http://localhost:8080/${props.vid}/commentpage`)
+        .then(response => {
+            comments.value = response.data.data
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+function flushMyComment() {
+    myComment.content = ''
+    myComment.point = ''
+}
+
+function pushComment() {
+    if (!userStore.loginState) {
+        alert("Please login first!")
+        flushMyComment()
+        return
+    }
+    if (!myComment.content || !myComment.point) {
+        alert("Empty comment or point is not allowed!")
+        flushMyComment()
+        return
+    }
+    axios.post(`http://localhost:8080/comment/${props.vid}`, {
+        vehicleID: props.vid,
+        content: myComment.content,
+        userID: userStore.UserID,
+        point: myComment.point
+    })
+        .then(response => {
+            console.log(response)
+            myComment.content = ''
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+getComments()
 
 </script>
 
 <template>
-    <div id="comments">
+    <div>
+        <div class="display">
+            <ul>
+                <li v-for="(comment, index) in comments" :key="index">
+                    <p>User ID: {{ comment.userID }}</p>
+                    <p>Point: {{ comment.point }}</p>
+                    <p>Comment: {{ comment.content }}</p>
+                </li>
+            </ul>
+        </div>
+
+        <div class="submit" v-if="userStore.loginState">
+            <h3> Summit your comment </h3>
+            <input v-model="myComment.content">
+            <h3> Give your point </h3>
+            <input v-model="myComment.point">
+            <br />
+            <button @click="pushComment()">Summit</button>
+        </div>
+
 
     </div>
 </template>
---
+++ src/components/PhotoWall.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const props = defineProps(['vid'])
+const comments = ref([]);
+
+function getComments() {
+    axios.get(`http://localhost:8080/${props.vid}/photowall`)
+        .then(result => {
+            photoWall.value = result.data.data
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+getComments()
+
+</script>
+
+<template>
+    <div>
+        <ul>
+            <li v-for="(comment, index) in comments" :key="index">
+                {{ comment }}
+            </li>
+        </ul>
+    </div>
+</template>
