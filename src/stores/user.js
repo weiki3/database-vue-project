8c9b5c0b0dd2c888ac6cce81791d203dadf1b6cf
weiki <wikii3@outlook.com>
2023-12-22 12:26:34 +0800

    v0.0.3 login page ready to use

--- src/stores/user.js
+++ src/stores/user.js
@@ -4,39 +4,47 @@ import { defineStore } from 'pinia'
 // 创建 store
 const useUserStore = defineStore('user', {
     persist: {
-        key: "USER",
+        key: "UserID",
         storage: sessionStorage,
-        paths: ["token"]
+        paths: ["password"]
     },
     // 定义状态：一个函数，返回一个对象
     state: () => ({
-        username: '',
-        token: ''
+        UserID: '',
+        password: '',
+        user_name: '',
+        email: '',
     }),
 
     // 定义 getters，等同于组件的计算属性
     getters: {
         // getter 函数接收 state 作为参数，推荐使用箭头函数
-        hello: state => 'Hello!' + state.username
+        hello: state => 'Hello!' + state.user_name
     },
 
     // 定义 actions，有同步和异步两种类型
     actions: {
         // 异步 action，一般用来处理异步逻辑
         async login(userData) {
-            const result = await axios.post('/users/login', userData)
-            const { data, code } = result.data
-            if (code === 0) {
+            const result = await axios.get(`/users/login?UserID=${userData.UserID}&password=${userData.password}`)
+            const { data, state } = result.data
+            console.log(result)
+            if (state === 200) {
                 // action 中修改状态
-                this.username = data.username
-                this.token = data.token
+                this.UserID = userData.UserID;
+                this.password = userData.password;
+                this.user_name = data.user_name;
+                this.email = data.email;
             }
+            console.log(this)
         },
 
         // 同步 action
         logout() {
-            this.token = ''
-            this.username = ''
+            this.password = ''
+            this.UserID = ''
+            this.user_name = ''
+            this.email = ''
         }
     }
 })
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,7 +3,7 @@ import { reactive } from 'vue'
 import useUserStore from "../stores/user"
 
 const userData = reactive({
-    username: '',
+    UserID: '',
     password: '',
 })
 
@@ -14,8 +14,8 @@ const onLogin = async () => {
     // 使用 actions，当作函数一样直接调用
     // login action 定义为了 async 函数，所以它返回一个 Promise
     await userStore.login(userData)
-    userData.username = ''
-    userData.password = ''
+    // userData.UserID = ''
+    // userData.password = ''
 }
 
 const onLogout = () => {
@@ -26,19 +26,23 @@ const onLogout = () => {
 <template>
     <div>
         <!-- state：通过 store 直接访问 -->
-        <template v-if="userStore.token">
-            {{ userStore.username }}
+        <template v-if="userStore.password">
+            {{ userStore.UserID }}
             <br />
             <button @click="onLogout">退 出</button>
         </template>
 
         <template v-else>
-            用户名：<input v-model="userData.username" />
+            用户名：<input v-model="userData.UserID" />
             <br />
             密码：<input v-model="userData.password" type="password" />
             <br />
             <button @click="onLogin">登 录</button>
         </template>
-
+        <div v-if="userStore.password">
+            <h2>{{ userStore.hello }}</h2>
+            <p>用户 ID：{{ userStore.UserID }}</p>
+            <p>用户 email：{{ userStore.email }}</p>
+        </div>
     </div>
 </template>
