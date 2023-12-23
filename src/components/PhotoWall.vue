9bddd8a8536c990c19a16e2e45fe0254180fadec
weiki <wikii3@outlook.com>
2023-12-23 14:59:40 +0800

    v0.0.14 fix upload photo part

--- src/components/PhotoWall.vue
+++ src/components/PhotoWall.vue
@@ -18,7 +18,25 @@ function getPhotoWall() {
             console.log(err)
         })
 }
-getPhotoWall()
+
+function uploadPhoto(e) {
+    let file = e.target.files[0];
+    let param = new FormData(); //创建form对象
+    param.append('File', file);//通过append向form对象添加数据
+    param.append('id', props.vid)
+    console.log(param.get('file')); //FormData私有类对象，访问不到，可以通过get判断值是否传进去
+    let config = {
+        headers: { 'Content-Type': 'multipart/form-data' } //这里是重点，需要和后台沟通好请求头，Content-Type不一定是这个值
+    }; //添加请求头
+    axios.post('https://localhost:8080/upload/photo', param, config)
+        .then(response => {
+            if (response.data.state !== 200) {
+                alert("Failed to upload!")
+            }
+        }).catch(() => {
+            alert("Failed to upload!")
+        })
+}
 
 </script>
 
@@ -35,12 +53,6 @@ getPhotoWall()
     </div>
     <div class="upload" v-if="userStore.loginState">
         <h3> Upload your photo </h3>
-        <div class="uploadPage">
-            <n-upload action="https://localhost:8080/upload/photo" :method="POST" :data="{
-                'id': props.vid
-            }">
-                <n-button>上传文件</n-button>
-            </n-upload>
-        </div>
+        <input type="file" class="file" name="avatar" accept="image/png, image/jpeg" @change="uploadPhoto" />
     </div>
 </template>
