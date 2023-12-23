f1f692d2fd638e45ef5d0980fa0af5343880c304
weiki <wikii3@outlook.com>
2023-12-23 21:27:53 +0800

    v0.0.26 fix onMount

--- src/components/PhotoWall.vue
+++ src/components/PhotoWall.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { NButton, NCard, NUpload, NImage, NDivider } from 'naive-ui';
 import axios from 'axios';
 import useUserStore from "../stores/user"
